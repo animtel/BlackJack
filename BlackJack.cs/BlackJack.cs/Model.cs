dafa12e80aacba853fa920548fa713fc21e2afff
Tel Anim <animtel@gmail.com>
2017-08-19 14:01:28 +0300

    added method, who generating deck

--- BlackJack.cs/BlackJack.cs/Model.cs
+++ BlackJack.cs/BlackJack.cs/Model.cs
@@ -8,5 +8,25 @@ namespace BlackJack.cs
 {
     class Model
     {
+        static Player[] players = new Player[5];
+        static int point = 0;
+
+        public static PlayingCard[] GenerateDeck()
+        {
+            PlayingCard[] deck = new PlayingCard[52]; // generate array of card
+            int counter = 0; // counter each card in array, for adding suit and value of card
+
+            for (int suit = 1; suit < 5; suit++)
+            {
+                for (int value = 0; value < 14; value++)
+                {
+                    deck[counter] = new PlayingCard(suit, value); // generate card with suit and value, and adding to array
+                    counter++;
+                }
+            }
+            return deck;
+        }
+
+
     }
 }
